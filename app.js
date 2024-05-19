d4cad26de9aadce3a1391f9067fd6a3e90269c93
Rameen Khan <162459141+RameenKhannnn@users.noreply.github.com>
2024-05-19 08:17:14 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+var weather = prompt("Enter your area weather:");
+if (weather == 'hot') {
+    document.write('<div class="card w-60 m-auto " style="width: 18rem;">' +
+    '<img src="heat_water_bottlepng.jpg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');}else{
+    document.write(" 😜 ")
+ }
+ if (weather == 'cold') {
+    document.write('<div class="card w-60 m-auto " style="width: 18rem;">' +
+    '<img src="images.jpg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');}
+ 
+ if (weather == 'sunny') {
+    document.write('<div class="card w-60 m-auto " style="width: 18rem;">' +
+    '<img src="sunnyyy.jpg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+}
+if (weather == 'cloudy') {
+    document.write('<div class="card w-60 m-auto " style="width: 18rem;">' +
+    '<img src="cloudy.avif" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+}
+if (weather == 'partly') {
+    document.write('<div class="card w-60 m-auto " style="width: 18rem;">' +
+    '<img src="PARTLY-CLOUDY.png" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+} 
+if (weather == 'rainy') {
+  document.write('<div class="card w-60 m-auto " style="width: 18rem;">' +
+  '<img src="mmmm.jpg" class="card-img-top rounded-5" alt="...">' +
+ ' <div class="card-body ">' +
+  '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+ ' </div>' +
+' </div>');
+}
+
+
+
+if (weather == 'stormy') {
+  document.write('<div class="card w-60 m-auto " style="width: 18rem;">' +
+  '<img src="ppp.webp" class="card-img-top rounded-5" alt="...">' +
+ ' <div class="card-body ">' +
+  '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+ ' </div>' +
+' </div>');
+}
+if (weather == 'windy') {
+  document.write('<div class="card w-60 m-auto " style="width: 18rem;">' +
+  '<img src="ooo.jpg" class="card-img-top rounded-5" alt="...">' +
+ ' <div class="card-body ">' +
+  '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+ ' </div>' +
+' </div>');
+}
+if (weather == 'snowy') {
+  document.write('<div class="card w-60 m-auto " style="width: 18rem;">' +
+  '<img src="images.jpg" class="card-img-top rounded-5" alt="...">' +
+ ' <div class="card-body ">' +
+  '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+ ' </div>' +
+' </div>');
+}
+
+
